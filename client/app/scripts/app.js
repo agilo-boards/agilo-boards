eeb893c252ea0bc23d074a6088ce7a9f22c3caae
adiherzog <adrian.herzog@gmail.com>
2014-07-01 15:04:23 +0200

    Formatting

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,23 +1,23 @@
 'use strict';
 
 angular
-  .module('agiloBoardsApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/scrumboard', {
-        templateUrl: 'views/scrumboard.html',
-        controller: 'ScrumboardCtrl'
-      })
-      .when('/backlog', {
-          templateUrl: 'views/backlogboard.html',
-          controller: 'BacklogboardCtrl'
-        })
-      .otherwise({
-        redirectTo: '/scrumboard'
-      });
-  });
+    .module('agiloBoardsApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/scrumboard', {
+                templateUrl: 'views/scrumboard.html',
+                controller: 'ScrumboardCtrl'
+            })
+            .when('/backlog', {
+                templateUrl: 'views/backlogboard.html',
+                controller: 'BacklogboardCtrl'
+            })
+            .otherwise({
+                redirectTo: '/scrumboard'
+            });
+    });

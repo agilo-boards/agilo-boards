09b8ad36133cc6e39ab3cb54fe957e0431c301a2
Fredericson <fredy.setz@gmx.ch>
2014-07-03 17:20:44 +0200

    fix the DateFields for Releases and Sprint Object

--- client/test/webPages/navigationBarPage.js
+++ client/test/webPages/navigationBarPage.js
@@ -13,44 +13,64 @@ function NavigationBarPage(overridePath) {
     this.reloadButton = element(by.id('reloadButton'));
     this.timeDone = element(by.binding('allTimeDone'));
     this.timeRemaining = element(by.binding('allTimeRemaining'));
+
+    this.description = element(by.id('releaseDescription'));
+    this.dueDate = element(by.id('dueDate'));
+    this.completedDate = element(by.id('completedDate'));
 }
 
 util.inherits(NavigationBarPage, BaseWebPage);
 
-NavigationBarPage.prototype.chooseSprintBranch = function(sprintName) {
+NavigationBarPage.prototype.chooseSprintBranch = function (sprintName) {
     // Open menu first, otherwise we cannot click
-    element(by.model('sprints')).element(by.xpath('//option[text() =\''+sprintName+'\']')).click();
+    element(by.model('sprints')).element(by.xpath('//option[text() =\'' + sprintName + '\']')).click();
 };
 
-NavigationBarPage.prototype.assertNumberOfSprints = function(listOfSprints) {
+NavigationBarPage.prototype.assertNumberOfSprints = function (listOfSprints) {
     var len = listOfSprints.length;
-    for (var i=0; i<len; i++) {
+    for (var i = 0; i < len; i++) {
         if (i in listOfSprints) {
-            expect(element(by.xpath('//select[@id="sprints"]/option['+ (1+i) +']')).getText()).toEqual(listOfSprints[i]);
+            expect(element(by.xpath('//select[@id="sprints"]/option[' + (1 + i) + ']')).getText()).toEqual(listOfSprints[i]);
         }
     }
 };
-NavigationBarPage.prototype.assertNumberOfReleases = function(listOfReleases) {
+NavigationBarPage.prototype.assertNumberOfReleases = function (listOfReleases) {
     var len = listOfReleases.length;
-    for (var i=0; i<len; i++) {
+    for (var i = 0; i < len; i++) {
         if (i in listOfReleases) {
-            expect(element(by.xpath('//select[@id="release"]/option['+ (1+i) +']')).getText()).toEqual(listOfReleases[i]);
+            expect(element(by.xpath('//select[@id="release"]/option[' + (1 + i) + ']')).getText()).toEqual(listOfReleases[i]);
         }
     }
 };
 
-NavigationBarPage.prototype.reload = function() {
+NavigationBarPage.prototype.reload = function () {
     this.reloadButton.click();
 };
 
-NavigationBarPage.prototype.assertTimeDone = function(expectedText) {
+NavigationBarPage.prototype.assertTimeDone = function (expectedText) {
     this.timeDone.getText().then(function (text) {
-        expect(text).toContain('done (with admin): '+ expectedText);
+        expect(text).toContain('done (with admin): ' + expectedText);
     });
 };
-NavigationBarPage.prototype.assertTimeRemaining = function(expectedText) {
+NavigationBarPage.prototype.assertTimeRemaining = function (expectedText) {
     this.timeRemaining.getText().then(function (text) {
         expect(text).toContain('remaining: ' + expectedText);
+    });
+};
+
+NavigationBarPage.prototype.assertDescription = function (expectedText) {
+    this.description.getText().then(function (text) {
+        expect(text).toContain(expectedText);
+    });
+};
+NavigationBarPage.prototype.assertDueDate = function (expectedText) {
+    this.dueDate.getText().then(function (text) {
+        expect(text).toContain('due Date : ' + expectedText);
+    });
+};
+NavigationBarPage.prototype.assertCompletedDate = function (expectedText) {
+    this.completedDate.getText().then(function (text) {
+        expect(text).toContain('completed Date: ' + expectedText);
     });
 };
 

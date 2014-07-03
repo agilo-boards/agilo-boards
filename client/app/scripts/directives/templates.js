7b3a8dff6112cc8d34388d28f8a473981ab1d999
Sandra Weber <s.weber@bluemail.ch>
2014-07-03 17:42:02 +0200

    Fixed link for opening ticket and editing ticket

--- client/app/scripts/directives/templates.js
+++ client/app/scripts/directives/templates.js
@@ -7,8 +7,7 @@ angular.module('agiloBoardsApp')
             templateUrl: 'templates/story.html',
             scope: {
                 backlogMode: '@',
-                compactMode: '@',
-                ownerMode: '@',
+                compactMode: '=',
                 story: '='
             }
         };
@@ -24,7 +23,40 @@ angular.module('agiloBoardsApp')
             restrict: 'E',
             templateUrl: 'templates/task.html',
             scope: {
-                task: '='
+                task: '=',
+                compactMode: '='
+            }
+        };
+    })
+    .directive('agiloLinks', function (AGILO_URL) {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/links.html',
+            compactMode: '@',
+            scope: {
+                id: '=',
+                compactMode: '='
+            },
+            link: function(scope) {
+                scope.getViewTicketUrl = function (id) {
+                    return AGILO_URL + '/ticket/' + id;
+                };
+
+                scope.getEditTicketUrl = function (id) {
+                    return scope.getViewTicketUrl(id) + '?pane=edit';
+                };
+            }
+        };
+    })
+    .directive('scaleToFit', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var text = attrs['scaleToFit'];
+                if (text.length > 8) {
+                    text = text.substr(0,7)+'..';
+                }
+                element.text(text);
             }
         };
     });
